9638aebaf610fd9285528ff960bdd39eee48056b
Mr.Liu <773890440@qq.com>
2023-01-13 02:47:37 +0800

    update

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import path from 'path'
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import dts from 'rollup-plugin-dts';
+import rollup from 'rollup';
+export default rollup.defineConfig([
+  {
+    input: './src/index.ts',
+    output: [
+      {
+        file: path.resolve('./dist/index.cjs'),
+        format: "cjs"
+      },
+      {
+        file: path.resolve('./dist/index.mjs'),
+        format: "es"
+      }
+    ],
+    plugins: [terser(), typescript(), babel()]
+  },
+  {
+    input: './src/index.ts',
+    plugins: [dts.default()],
+    output: {
+      format: 'esm',
+      file: 'dist/index.d.ts',
+    }
+  }
+])
